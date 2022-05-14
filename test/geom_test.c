7297b7b2c13a8141e88406823304a0b9c8c42932
Ayusha Mutuev <buuzytrash@users.noreply.github.com>
2022-05-14 21:45:46 +0800

    Function get_numbers tested

--- test/geom_test.c
+++ test/geom_test.c
@@ -16,3 +16,37 @@ CTEST(figcheck, test)
 
     fig_check(fig, "circle");
 }
+
+CTEST(getnumbers, test_1)
+{
+    krug circle;
+    char str[] = "circle(10 10, 10)";
+
+    int error = 0;
+    circle.error = 0;
+    float x = 10, y = 10, rad = 10;
+
+    get_numbers(&circle, str);
+
+    ASSERT_DBL_NEAR(circle.p.x, x);
+    ASSERT_DBL_NEAR(circle.p.y, y);
+    ASSERT_DBL_NEAR(circle.r, rad);
+    ASSERT_EQUAL(circle.error, error);
+}
+
+CTEST(getnumbers, test_2)
+{
+    krug circle;
+    char str[] = "circle(-4 2, 3.5) af";
+
+    int error = 1;
+    circle.error = 0;
+    float x = -4, y = 2, rad = 3.5;
+
+    get_numbers(&circle, str);
+
+    ASSERT_DBL_NEAR(circle.p.x, x);
+    ASSERT_DBL_NEAR(circle.p.y, y);
+    ASSERT_DBL_NEAR(circle.r, rad);
+    ASSERT_EQUAL(circle.error, error);
+}
