4ae8ec8fda8b0b9704fc725f159b812614a9b301
Ayusha Mutuev <buuzytrash@users.noreply.github.com>
2022-05-14 21:10:19 +0800

    First test created

--- test/geom_test.c
+++ test/geom_test.c
@@ -1,7 +1,18 @@
 #include <ctest.h>
 #include <libgeom/geom.h>
 
-CTEST(arithmetic_suite, simple_sum)
+CTEST(getfig, test)
 {
-    ASSERT_STR("str", "str");
+    char str[] = "circle(1.5 3, 5)", fig[10];
+    get_fig(str, fig);
+
+    ASSERT_STR(fig, "circle");
+}
+
+CTEST(figcheck, test)
+{
+    char str[] = "circle(1.5 3, 5)", fig[10];
+    get_fig(str, fig);
+
+    fig_check(fig, "circle");
 }
